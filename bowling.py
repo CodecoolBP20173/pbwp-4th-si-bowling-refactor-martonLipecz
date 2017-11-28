4128ad34bab1519c6e3caf8ffc87bbb913bdfbe8
Márton Lipécz <mlipecz@gmail.com>
2017-11-28 10:26:24 +0100

    autoformat

--- bowling.py
+++ bowling.py
@@ -3,22 +3,22 @@ def score(game):
     frame = 1
     in_first_half = True
     for i in range(len(game)):
-        
+
         if game[i] == '/':
             result += 10 - last
         else:
             result += get_value(game[i])
-        
-        if frame < 10  and get_value(game[i]) == 10:
+
+        if frame < 10 and get_value(game[i]) == 10:
             if game[i] == '/':
-                result += get_value(game[i+1])
+                result += get_value(game[i + 1])
             elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
+                result += get_value(game[i + 1])
+                if game[i + 2] == '/':
+                    result += 10 - get_value(game[i + 1])
                 else:
-                    result += get_value(game[i+2])
-        
+                    result += get_value(game[i + 2])
+
         last = get_value(game[i])
         if not in_first_half:
             frame += 1
@@ -29,8 +29,9 @@ def score(game):
         if game[i] == 'X' or game[i] == 'x':
             in_first_half = True
             frame += 1
-    
+
     return result
+
 
 def get_value(char):
     if char == '1' or char == '2' or char == '3' or \
