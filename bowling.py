5fc60d8d50ef3b4b689603aec25695657c9a4e60
Márton Lipécz <mlipecz@gmail.com>
2017-11-28 10:21:43 +0100

    delete comment, add couple new lines

--- bowling.py
+++ bowling.py
@@ -3,12 +3,12 @@ def score(game):
     frame = 1
     in_first_half = True
     for i in range(len(game)):
+        
         if game[i] == '/':
             result += 10 - last
         else:
             result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
+        
         if frame < 10  and get_value(game[i]) == 10:
             if game[i] == '/':
                 result += get_value(game[i+1])
@@ -18,6 +18,7 @@ def score(game):
                     result += 10 - get_value(game[i+1])
                 else:
                     result += get_value(game[i+2])
+        
         last = get_value(game[i])
         if not in_first_half:
             frame += 1
@@ -28,6 +29,7 @@ def score(game):
         if game[i] == 'X' or game[i] == 'x':
             in_first_half = True
             frame += 1
+    
     return result
 
 def get_value(char):
