038f57592c61baf4742571273f14d71dd1fa00de
Márton Lipécz <mlipecz@gmail.com>
2017-11-28 10:32:10 +0100

    get_value conditional

--- bowling.py
+++ bowling.py
@@ -34,9 +34,7 @@ def score(game):
 
 
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+    if char in ('1', '2', '3', '4', '5', '6', '7', '8', '9'):
         return int(char)
     elif char == 'X' or char == 'x':
         return 10
